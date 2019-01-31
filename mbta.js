b9b2d9416cd5355dfebe2a132981d0a2ca3c8fa1
asholds <asholds@gmail.com>
2019-01-31 14:30:29 -0500

    Update mbta.js

--- mbta.js
+++ mbta.js
@@ -1,176 +1,91 @@
-//TODO - sort stations by lat/lon distance
+var MBTA_DATA = {}
 
-var App = angular.module('App', ['ngMaterial']);
-
-App.controller('MainController', ['$scope', 'predictions', function($scope, predictions) {
+$(document).ready(function(){
   
-  $scope.showAlert = function(dir_str){
-    $scope.view_direction = dir_str;
+  navigator.geolocation.getCurrentPosition(function(position){
+    queryMBTA(position.coords)
+  })
 
-    if(dir_str == "Northbound"){
-      $( "span:contains('Northbound')" ).parent().addClass('md-accent');
-      $( "span:contains('Southbound')" ).parent().removeClass('md-accent');
-    }else{
-      $( "span:contains('Southbound')" ).parent().addClass('md-accent');
-      $( "span:contains('Northbound')" ).parent().removeClass('md-accent');
+  $('.switchDirection').click(function(e){
+    var newDirection = $('#disabledDirection').html()
+    $('#disabledDirection').html($('#activeDirection').html())
+    $('#activeDirection').html(newDirection)
+    displayMBTA(MBTA_DATA[newDirection])
+
+  })
+})
+
+function queryMBTA(coords){
+  var API_KEY = "161d100130e44ab2ab9a5ab5e80391b4"
+  var API_URL = "https://api-v3.mbta.com/" +
+      "predictions" + 
+      "?filter[latitude]=" + coords.latitude + 
+      "&filter[longitude]=" + coords.longitude +
+      "&filter[radius]=0.03" +
+      "&filter[route_type]=1" +
+      "&include=stop,trip,route" + 
+      "&api_key=" + API_KEY
+
+  $.ajax({
+    url: API_URL
+  }).done(function(response){
+    MBTA_DATA = parseMBTA(response)
+    displayMBTA(MBTA_DATA["Alewife"])
+  })
+}
+
+function parseMBTA(response){
+//  console.log(response)
+
+  var stops = {}
+  $.each(response.included.filter(stop => stop.type == "stop"), function(i,stop){ stops[stop.id] = stop.attributes })
+
+  var trips = {}
+  $.each(response.included.filter(trip => trip.type == "trip"), function(i,trip){ trips[trip.id] = trip.attributes })
+
+  var predictions = {}
+  $.each(response.data.filter(prediction => prediction.relationships.route.data.id=="Red"), function(i,prediction){ 
+    if(trips[prediction.relationships.trip.data.id].headsign != "Ashmont" && !["Savin Hill","Fields Corner","Shawmut","Ashmont"].includes(stops[prediction.relationships.stop.data.id].name)){ //ignore ashmont side stations
+      predictions[prediction.id] = prediction.attributes 
+      predictions[prediction.id].trip = trips[prediction.relationships.trip.data.id] 
+      predictions[prediction.id].headsign = trips[prediction.relationships.trip.data.id].headsign
+      predictions[prediction.id].stop = stops[prediction.relationships.stop.data.id]
+      predictions[prediction.id].stopName = stops[prediction.relationships.stop.data.id].name
     }
-  };
+  })
 
-  $scope.dataLoaded = false;
-
-  var vm = this;
-  $scope.view_direction = "Northbound";
+//  console.log(predictions)
   
+  var stations = {Alewife:{},Braintree:{}}
+  $.each(predictions, function(i,prediction){
 
-  $scope.queryMBTA = function(){ 
-    $scope.dataLoaded = false;
-    predictions.get(function (data) {
-      
-      //raw data from MBTA
-      window.console && console.log(data);
-
-      vm.alerts = [];
-      $.each(data.alert_headers, function(){ 
-        if(this.effect_name != "Station Issue" && this.effect_name != "Station Closure"){
-          vm.alerts.push(this.header_text);
-        }
-      });
-      
-      //navigator.geolocation.getCurrentPosition(function(location) {
-      //var USER_LAT = location.coords.latitude;
-      //var USER_LON = location.coords.longitude;
-  
-
-      vm.stations = [];
-
-      var stationData = [
-        {'name': 'BRAINTREE',         'display': true, 'lat': 42.2087804241, 'lon': -71.0013341904 },
-        {'name': 'QUINCY ADAMS',      'display': true, 'lat': 42.2327515704, 'lon': -71.0071492195 },
-        {'name': 'QUINCY CENTER',     'display': true, 'lat': 42.250879, 'lon': -71.004798 },
-        //{'name': 'Wollaston',         'display': true, 'lat': 42.2656146622, 'lon': -71.0194015503 },
-        {'name': 'NORTH QUINCY',      'display': true, 'lat': 42.2748161177, 'lon': -71.0291755199 },
-        {'name': 'JFK/UMASS',         'display': true, 'lat': 42.3214378629, 'lon': -71.0523927212 },
-        {'name': 'ANDREW',            'display': true, 'lat': 42.32955, 'lon': -71.05696 },
-        {'name': 'BROADWAY',          'display': true, 'lat': 42.3429, 'lon': -71.05713 },
-        {'name': 'SOUTH STATION',     'display': true, 'lat': 42.351709611, 'lon': -71.0549998283 },
-        {'name': 'DOWNTOWN CROSSING', 'display': true, 'lat': 42.355295, 'lon': -71.060788 },
-        {'name': 'PARK STREET',       'display': true, 'lat': 42.3561971861, 'lon': -71.0622954369 },
-        {'name': 'CHARLES/MGH',       'display': true, 'lat': 42.3612710899, 'lon': -71.0720801353 },
-        {'name': 'KENDALL/MIT',       'display': true, 'lat': 42.3624602268, 'lon': -71.0865855217 },
-        {'name': 'CENTRAL',           'display': true, 'lat': 42.3651634477, 'lon': -71.103322506 },
-        {'name': 'HARVARD',           'display': true, 'lat': 42.373939, 'lon': -71.119106 },
-        {'name': 'PORTER',            'display': true, 'lat': 42.3883461218, 'lon': -71.1192440987 },
-        {'name': 'DAVIS',             'display': true, 'lat': 42.3960638548, 'lon': -71.1220550537 },
-        {'name': 'ALEWIFE',           'display': true, 'lat': 42.3949070477, 'lon': -71.1409807205 },
-        {'name': 'SAVIN HILL',        'display': false, 'lat': 0, 'lon': 0 },
-        {'name': 'FIELDS CORNER',     'display': false, 'lat': 0, 'lon': 0 },
-        {'name': 'SHAWMUT',           'display': false, 'lat': 0, 'lon': 0 },
-        {'name': 'ASHMONT',           'display': false, 'lat': 0, 'lon': 0 }
-      ];
-      
-      $.each(stationData, function(i,v){
-        var station = {};
-        station.name = v.name;
-        station.predictions = [];
-        station.display = v.display;
-        var lat = v.lat;
-        var lon = v.lon;
-        
-        station.calcDist = function(lat1, lon1, lat2, lon2) 
-        {
-          var R = 6371; // km
-          var dLat = (lat2-lat1) * Math.PI / 180;
-          var dLon = (lon2-lon1) * Math.PI / 180;
-          var lat1 = (lat1) * Math.PI / 180;
-          var lat2 = (lat2) * Math.PI / 180;
-
-          var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-          var d = R * c;
-          return d;
-        }
-
-        station.distance = station.calcDist($scope.USER_LAT, $scope.USER_LON, v.lat, v.lon);
-
-        vm.stations.push(station);
-      });
-
-    
-      //, "Quincy Adams", "Quincy Center", "Wollaston", "North Quincy", "JFK/UMASS", "Andrew", "Broadway", "South Station", "Downtown Crossing", "Park Street", "Charles/MGH", "Kendall/MIT", "Central", "Harvard", "Porter", "Davis", "Alewife"
-
-      //console.log(vm.stations);
-
-      $.each(data.direction, function(){ 
-        var direction = this;
-        $.each(direction.trip, function(){  //each train
-          var train = this;
-
-          $.each(train.stop, function(){ 
-            var stop = this;
-            var prediction = {};
-            prediction.headsign = train.trip_headsign;
-            prediction.direction = direction.direction_name;
-            prediction.distance = this.pre_away;
-
-            var stopName = stop.stop_name.split(' - ')[0].toUpperCase();
-            vm.stations.filter(function( obj ) { return obj.name == stopName })[0].predictions.push(prediction);
-            
-          });
-        });
-      });
-      //console.log(vm.stations);
-
-      vm.stations.sort(function(a,b){
-        return a.distance - b.distance;
-      });
-
-      $.each(vm.stations, function(station){
-        this.predictions.sort(function(a, b)
-          {
-            return a.distance - b.distance;
-          });
-      });
-      window.console && console.log(vm.stations);
-      $scope.dataLoaded = true;
-    });
-  };
-
-
-  navigator.geolocation.getCurrentPosition(function(location) {
-    $scope.USER_LAT = location.coords.latitude;
-    $scope.USER_LON = location.coords.longitude;
-    $scope.queryMBTA();
-  });
-
-}]);
-
-App.factory('predictions', ['$http', function ($http) {
-  return {
-    get: function (callback) {
-      $http.get('https://realtime.mbta.com/developer/api/v2/predictionsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=Red&format=json').success(function (data) { callback(data); })
+    if(!stations[prediction.headsign][prediction.stopName]){ 
+      stations[prediction.headsign][prediction.stopName] = [] 
     }
-  }
-}]);
 
-App.filter('toTimeDisplay', function(){
-  return function(seconds){
-    var minutes = parseInt(seconds/60, 10);
-    if(minutes<1){return "ARR"}
-    else{return minutes + " min"}
-  }
-})
+    var now = new Date();
+    var arrival = new Date(prediction.arrival_time)
+    var timeDiff = Math.abs(arrival.getTime() - now.getTime())
+    var minutesAway = Math.ceil(timeDiff/(1000*60))
 
-App.filter('toCaps', function(){
-  return function(str){
-    return str.toUpperCase();
-  }
-})
+    stations[prediction.headsign][prediction.stopName].push({
+      "arrival_time": minutesAway
+    })
+  })
 
-App.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('dark-grey')
-    .primaryPalette('grey') 
-    .accentPalette('red')
-    .dark();
-})
+//  console.log(stations)
 
+  return stations
+}
+
+function displayMBTA(stations){
+  console.log(stations)
+  $('#app').html("")
+  $.each(stations, function(i,station){
+    $('#app').append('<h2>' + i + '</h2><ul>')
+      $.each(station.slice(0,3), function(i,train){
+        $('#app').append('<li>' + train.arrival_time + ' minutes</li>')
+      })
+    $('#app').append('</ul>')
+  })
+}
